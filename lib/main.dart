353fc9c1c8c4e7f589813e5f7ce7d6e5fc532689
Aleksandr Bogdan <bogdan@sw-expert.com>
2023-05-08 20:29:49 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Open Sans'),
       ),
+      //
       home: HomeView(),
     );
   }
